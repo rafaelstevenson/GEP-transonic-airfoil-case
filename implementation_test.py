3d2d8d7e8c68b27ec90241b7ee65ce6c3e5e414f
Rafael Stevenson <rafaelstevensong@gmail.com>
2022-02-25 13:53:44 +0700

    Running GEP Trial, but still bad result

--- implementation_test.py
+++ implementation_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from GEP import GeneExpressionProgramming
 
@@ -5,7 +6,23 @@ from GEP import GeneExpressionProgramming
 df = pd.read_csv('../testing_datasets/transonic_airfoil_data_set.csv')
 print(df.head())
 
-func_set = ['+','-','*','/', '(sqrt)']
+class MinMaxScaler:
+    def fit_transform(self, arr):
+        self.max = max(arr)
+        self.min = min(arr)
+        arr = arr-self.min/(self.max-self.min)
+        return arr
+    def transform(self, arr):
+        arr = arr-self.min/(self.max-self.min)
+        return arr
+
+#scaler_x = MinMaxScaler()
+#scaler_y = MinMaxScaler()
+#df['AoA'] = scaler_x.fit_transform(df['AoA'])
+#df['Cd'] = scaler_y.fit_transform(df['Cd'])
+
+
+func_set = ['+','-','*','/', '(sqrt)','(exp)', '(ln)']
 
 term_set = ['a']
 operator_probabilities = {
@@ -15,9 +32,9 @@ operator_probabilities = {
 }
 
 head_length = 7
-population_size = 100
-generations = 5
-fitness_func = 'mse'
+population_size = 300
+generations = 20
+fitness_func = 'r2'
 
 GEPProcess = GeneExpressionProgramming(head_length,func_set,term_set,operator_probabilities)
 GEPProcess.RunGEP(df['AoA'],df['Cd'],population_size,generations,fitness_func)
---
+++ prediction_vs_truth.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+from sklearn.metrics import mean_squared_error, r2_score
+
+one_arity_func = ['(sqrt)', '(sin)', '(exp)', '(ln)', '(inv)', '(gau)']
+two_arity_func = ['+', '-', '*', '/']
+
+def ChromToET(chromosome):
+    '''Take a string of chromosome change it to
+    a dictionary of expression tree{row:element_on_row}'''
+    expr_tree = {0: [chromosome[0]]}
+
+    i = 1
+    start_counter = 1
+    while True:
+
+        take_next = 0
+        terminal_row = True
+
+        for element in expr_tree[i - 1]:
+            if element in two_arity_func:
+                terminal_row = False
+                take_next += 2
+            elif element in one_arity_func:
+                terminal_row = False
+                take_next += 1
+
+        if terminal_row==True:
+            break
+
+        last_counter = start_counter + take_next
+        next_elem = chromosome[start_counter:last_counter]
+        expr_tree[i] = next_elem
+        start_counter = last_counter
+
+        i += 1
+
+    return expr_tree
+
+
+def EvaluateET(chromosome, variable_dict):
+    '''Take chromosome and terminal variable dictionary{'symbol':value}
+    and perform calculation from the chromosome->ET->calculation->prediction'''
+
+    # Change string to list in each row of ET and change variables to sample value
+    expr_tree = ChromToET(chromosome)
+    for i in range(len(expr_tree)):  # iterate rows
+        el = 0
+        for element in expr_tree[i]:  # iterate elements in a row
+            if element in variable_dict.keys():
+                expr_tree[i][el] = str(variable_dict[element])
+            el += 1
+
+    def operate_two_arity(representation, a, b):
+        a = float(a)
+        b = float(b)
+
+        if representation=='+':
+            result = a + b
+        elif representation=='-':
+            result = a - b
+        elif representation=='*':
+            result = a * b
+        elif representation=='/':
+            if b==0:
+                b = 1e-6
+            result = a / b
+
+        return str(result)
+
+    def operate_one_arity(representation, a):
+        a = float(a)
+
+        if representation=='(sqrt)':
+            if a >= 0:
+                result = a ** 0.5
+            else:
+                result = (abs(a)) ** 0.5
+            # result = math.sqrt(a)
+
+        elif representation=='(sin)':
+            result = math.sin(a)
+        elif representation=='(exp)':
+            try:
+                result = math.exp(a)
+            except:
+                result = 1e6
+        elif representation=='(ln)':
+            if a==0:
+                a = 1e-6
+            elif a < 0:
+                a = abs(a)
+            result = math.log(a, math.e)
+        elif representation=='(inv)':
+            if a==0:
+                a = 1e-6
+            result = 1 / a
+        elif representation=='(gau)':
+            result = np.random.normal(1)
+
+        return str(result)
+
+    for row in range(len(expr_tree) - 2, -1, -1):  # iterate rows from second last row to root
+        i = 0
+        for element in expr_tree[row]:
+            if element in two_arity_func:
+                a = expr_tree[row + 1][0]
+                b = expr_tree[row + 1][1]
+
+                result = operate_two_arity(element, a, b)
+                # buang 2 elemen pertama di row+1 dan replace elemen pertama di row
+                expr_tree[row + 1] = expr_tree[row + 1][2:]
+                expr_tree[row][i] = result
+
+            elif element in one_arity_func:
+                a = expr_tree[row + 1][0]
+                result = operate_one_arity(element, a)
+                expr_tree[row + 1] = expr_tree[row + 1][1:]
+                expr_tree[row][i] = result
+
+            i += 1
+
+    prediction = float(expr_tree[0][0])
+    return prediction
+
+
+# load dataset to get ground truth
+df = pd.read_csv('../testing_datasets/transonic_airfoil_data_set.csv')
+y_true = df['Cd']
+x = df['AoA']
+
+# declare the chromosome and the variable dictionary
+chromosome = ['/', 'a', '(exp)', '+', '(sqrt)', '*', '/', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+term_set = ['a']
+
+
+
+y_true = df['Cd']
+
+# Perform Prediction
+y_pred = []
+for i in range(len(pd.DataFrame(x))):
+    variable_dict = {}
+    nth_input = 0
+    for term in term_set:
+        variable_dict[term] = pd.DataFrame(x).iloc[i, nth_input]
+        nth_input += 1
+    prediction = EvaluateET(chromosome, variable_dict)
+    y_pred.append(prediction)
+
+y_pred = np.array(y_pred)
+
+# get metric of predictions
+print(f'Prediction Mean Squared Error (MSE): {mean_squared_error(y_true, y_pred)}')
+print(f'Prediction R-squared Score (R2): {r2_score(y_true, y_pred)}')
+
+plt.scatter(x, y_pred)
+plt.scatter(x, y_true)
+plt.show()
+print(y_pred)
